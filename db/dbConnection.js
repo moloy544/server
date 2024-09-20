98a4455e515635ee1a8dca4dc83d8000698e164c
Sanjoy Rakshit <sanoyrakshit@gmail.com>
2024-09-20 21:14:00 +0530

    add backup mongodb url and handle user connection limit

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,18 +1,26 @@
-import { connect } from 'mongoose';
+import mongoose from 'mongoose';
 
-let isConnected = false;
+let cachedConnection = null; // Cache the connection globally
 
 const connectToDatabase = async () => {
-  if (isConnected) {
-    return;
+  // If cachedConnection exists and is already connected, reuse it
+  if (cachedConnection && mongoose.connection.readyState === 1) {
+    return cachedConnection;
   }
 
   try {
-    const connectionInstance = await connect(process.env.DB_CONNECTION_URL);
-    isConnected = true;
-    console.log(`MongoDB connected to host: ${connectionInstance.connection.host}`);
+    // Connect to the database with a max pool size of 150 and max idle time of 10 minutes
+    cachedConnection = await mongoose.connect(process.env.DB_CONNECTION_URL, {
+      maxPoolSize: 150,         // Set maximum number of connections in the pool
+      maxIdleTimeMS: 600000     // Close idle connections after 10 minutes
+    });
+
+    console.log(`MongoDB connected to host: ${cachedConnection.connection.host}`);
+
+    return cachedConnection; // Return the cached connection
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
+    throw error; // Rethrow the error to handle it in the calling context
   }
 };
 
