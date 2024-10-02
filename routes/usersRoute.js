4ab3fbf315fd9670ea5a307bde0c4164a462f52c
Sanjoy Rakshit <sanoyrakshit@gmail.com>
2024-10-03 03:50:48 +0530

    improve report route

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -2,6 +2,14 @@ import { Router } from "express";
 import Reports from "../models/Reports.Model.js";
 import Movies from "../models/Movies.Model.js";
 import { parseCookies } from "../utils/index.js";
+import crypto from "crypto"
+
+// Utility to generate a 15-digit random ID
+const generateRandomId = () => {
+    const part1 = crypto.randomInt(1000000000, 9999999999);  // 10-digit number
+    const part2 = crypto.randomInt(10000, 99999);            // 5-digit number
+    return `${part1}${part2}`;                               // Concatenate both parts
+};
 
 const router = Router();
 
@@ -49,14 +57,26 @@ router.post('/action/report', async (req, res) => {
         const cookies = parseCookies(req);
 
         //Check if user is have userid or not
-        const userId = cookies['moviesbazar_user'];
+        let userId = cookies['moviesbazar_user'];
         if (!userId) {
-            return res.status(401).json({ message: 'Unauthorized action not allowed' });
+            // Generate a 15-digit random user ID
+            userId = generateRandomId();
+
+            // Check if the environment is production
+            const isProduction = process.env.NODE_ENV === 'production';
+
+            // Set the 'user' cookie with the random ID
+            res.cookie('moviesbazar_user', userId, {
+                path: '/', // The cookie is available throughout the site
+                sameSite: isProduction ? 'none' : 'lax', // Cookie security
+                secure: isProduction, // Secure cookie in production
+                httpOnly: true, // Prevent client-side JS from accessing the cookie
+            });
         };
 
         //Create a new report object with user id and report data
         const newReport = new Reports({ ...reportData, user: userId });
-           
+
         const saveReport = await newReport.save();
 
         if (saveReport) {
