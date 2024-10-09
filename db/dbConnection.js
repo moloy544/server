c8f688ca07413bfd282d7fc8e48ca61caf2d5f3c
Sanjoy Rakshit <sanoyrakshit@gmail.com>
2024-10-09 12:08:20 +0530

    add maxIdleTimeMS option in db connaction

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -3,12 +3,10 @@ import { connect } from 'mongoose';
 const connectToDatabase = async () => {
 
   try {
-    const connectionInstance = await connect(process.env.DB_CONNECTION_URL);
-
-    /** const connectionInstance = await connect(process.env.DB_CONNECTION_URL, {
-      maxPoolSize: 300,
+    const connectionInstance = await connect(process.env.DB_CONNECTION_URL,{
       maxIdleTimeMS: 600000 * 6     // Close idle connections after 1 hours
-    }); **/
+    });
+
     console.log(`MongoDB is connected to moviesbazar db host :${connectionInstance.connection.host}`);
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
